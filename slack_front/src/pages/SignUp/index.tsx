9750f2fdf2af02b75ed177a19a3bbce1dfb08a1a
titch <sujinlee0202@gmail.com>
2022-11-15 21:18:54 +0900

    Axios를 통한 비동기 처리

--- slack_front/src/pages/SignUp/index.tsx
+++ slack_front/src/pages/SignUp/index.tsx
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import React, { useState, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import useInput from '../../hooks/useInput';
-import { Header, Form, Label, Input, Button, LinkContainer, Error } from './styles';
+import { Header, Form, Label, Input, Button, LinkContainer, Error, Success } from './styles';
 
 const SignUp = () => {
   const [email, onChangeEmail] = useInput('')
@@ -10,6 +10,8 @@ const SignUp = () => {
   const [password, setPassword] = useState('')
   const [passwordCheck, setPasswordCheck] = useState('')
   const [mismatchError, setMismatchError] = useState(false)
+  const [signUpError, setSignUpError] = useState('')
+  const [signUpSuccess, setSignUpSuccess] = useState(false)
 
   const onChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value)
@@ -26,6 +28,8 @@ const SignUp = () => {
     console.log(email, nickname, password, passwordCheck)
     if(!mismatchError) { // false -> error, true -> pass
       console.log('서버 통과!!! 회원가입에 성공했습니다.')
+      setSignUpError('') // 초기화
+      setSignUpSuccess(false)
       axios.post('localhost:3095/api/users', {
         email,
         nickname,
@@ -33,9 +37,15 @@ const SignUp = () => {
       })
       .then((response) => {
         console.log(response)
+        setSignUpSuccess(true)
       }) // 성공
-      .catch((err) => {
-        console.log(err.response) 
+      .catch((error: Error | AxiosError) => {
+        if(axios.isAxiosError(error)) {
+          console.log(error.response)
+          setSignUpError(error.response?.data)
+        } else {
+          // stock
+        }
       }) // 실패
       .finally(() => {}) // 성공, 실패 여부와 상관없이 무조건 실행
     }
@@ -74,6 +84,8 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>} {/* mismatchEr = false일 때 <Error> */}
           {!nickname && <Error>닉네임이 없습니다.</Error>} {/* nickname이 false일 때 (!nickname) <Error> */}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되없습니다! 로그인해주세요</Success>}
         </Label>
         <Button type='submit'>
           회원가입
