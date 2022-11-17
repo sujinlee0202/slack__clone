d64263e19f1abce9aaf268e8a898796d4002e229
titch <sujinlee0202@gmail.com>
2022-11-17 11:35:31 +0900

    로그인 페이지

--- slack_front/src/pages/LogIn/index.tsx
+++ slack_front/src/pages/LogIn/index.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useCallback, useState } from 'react'
+import { Link } from 'react-router-dom';
+import useInput from '../../hooks/useInput';
+import { Header, Form, Label, Input, Button, LinkContainer, Error } from '../SignUp/styles';
 
 const LogIn = () => {
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const [loginError, setLoginError] = useState(false)
+
+  const onSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoginError(false)
+    axios.post('http://localhost/3095/api/users/login', {
+      email,
+      password,
+    })
+    .then(() => {})
+    .catch((error) => {
+      setLoginError(error.response?.data?.statusCode ===  401)
+    })
+  }, [email, password])
+
   return (
-    <div>Log In</div>
+    <div id='contaioner'>
+    <Header>Sleact</Header>
+    <Form onSubmit={onSubmit}>
+      <Label id='email-label'>
+        <span>이메일 주소</span>
+        <div>
+          <Input type='email' id='email' name='email' value={email} onChange={onChangeEmail}></Input>
+        </div>
+      </Label>
+      <Label id='password-label'>
+        <span>비밀번호</span>
+        <div>
+          <Input type='password' id='password' name='password' value={password} onChange={onChangePassword}></Input>
+        </div>
+        {loginError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+      </Label>
+      <Button type='submit'>로그인</Button>
+    </Form>
+    <LinkContainer>
+      아직 회원이 아니신가요? 
+      <Link to='/signup'>회원가입 하러가기</Link>
+    </LinkContainer>
+    </div>
   )
 }
 
