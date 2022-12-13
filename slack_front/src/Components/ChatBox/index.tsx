c5c0ce168325c86cd20698aadbaca63720dc43d0
titch <sujinlee0202@gmail.com>
2022-12-14 02:04:11 +0900

    Mention 기능 추가

--- slack_front/src/Components/ChatBox/index.tsx
+++ slack_front/src/Components/ChatBox/index.tsx
@@ -1,16 +1,29 @@
 import autosize from 'autosize'
 import React, { useCallback, useEffect, useRef } from 'react'
-import { ChatArea, Form, MentionsTextarea, SendButton, Toolbox } from './styles'
+import { ChatArea, EachMention, Form, MentionsTextarea, SendButton, Toolbox } from './styles'
+import { Mention, SuggestionDataItem } from 'react-mentions'
+import { useParams } from 'react-router-dom'
+import useSWR from 'swr'
+import { IUser, IUserWithOnline } from '../../typings/db'
+import fetcher from '../../utils/fetcher'
+import gravatar from 'gravatar'
 
 interface Props {
   chat: string
-  onSubmitForm: (e: React.FormEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>) => void
-  onChangeChat: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+  onSubmitForm: (e: any) => void
+  onChangeChat: (e: any) => void
   placeholder?: string
 }
 
 const ChatBox = ({ chat, onSubmitForm, onChangeChat, placeholder }: Props) => {
   const textareaRef = useRef<HTMLTextAreaElement>(null)
+
+  const { workspace } = useParams();
+  const { data: userData } = useSWR<IUser>(`/api/users`, fetcher, {
+    dedupingInterval: 2000,
+  })
+  const { data: memberData } = useSWR<IUserWithOnline[]>(userData ? `/api/workspaces/${workspace}/members` : null, fetcher)
+
 
   useEffect(() => {
     if(textareaRef.current) {
@@ -18,7 +31,7 @@ const ChatBox = ({ chat, onSubmitForm, onChangeChat, placeholder }: Props) => {
     }
   }, [])
 
-  const onKeyDownChat = useCallback((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  const onKeyDownChat = useCallback((e: React.KeyboardEvent<HTMLTextAreaElement> | React.KeyboardEvent<HTMLInputElement>) => {
     // enter : submit, shift+enter : 줄바꿈
     if(e.key === 'Enter') {
       if(!e.shiftKey) {
@@ -27,6 +40,24 @@ const ChatBox = ({ chat, onSubmitForm, onChangeChat, placeholder }: Props) => {
       }
     }
   }, [onSubmitForm])
+
+  const renderSuggestion: (
+    suggestion: SuggestionDataItem, 
+    search: string, 
+    highlightedDisplay: React.ReactNode, 
+    index: number, 
+    focused: boolean
+    ) => React.ReactNode = useCallback((
+      member, search, highlightedDisplay, index, focused
+    ) => {
+    if(!memberData) return;
+    return (
+      <EachMention focus={focused}>
+        <img src={gravatar.url(memberData[index].email, {s: '20px', d: 'retro'})} alt={memberData[index].nickname}></img>
+        <span>{highlightedDisplay}</span>
+      </EachMention>
+    )
+  }, [memberData])
   
   return (
     <ChatArea>
@@ -36,8 +67,18 @@ const ChatBox = ({ chat, onSubmitForm, onChangeChat, placeholder }: Props) => {
           onChange={onChangeChat}
           onKeyDown={onKeyDownChat}
           placeholder={placeholder}
-          ref={textareaRef}
-        />
+          inputRef={textareaRef}
+          allowSuggestionsAboveCursor
+        >
+          <Mention
+            appendSpaceOnAdd
+            trigger='@'
+            data={memberData?.map((item) => (
+              {id: item.id, display: item.nickname}
+            )) || []}
+            renderSuggestion={renderSuggestion}
+          />
+        </MentionsTextarea>
         <Toolbox>
           <SendButton className={
               'c-button-unstyled c-icon_button c-icon_button--light c-icon_button--size_medium c-texty_input__button c-texty_input__button--send' +
