2e48e2cbaeee933e3b2549a727f403f138bfb51c
titch <sujinlee0202@gmail.com>
2022-12-14 18:01:05 +0900

    mention 링크 기능 설정 with regexify-string

--- slack_front/src/Components/Chat/index.tsx
+++ slack_front/src/Components/Chat/index.tsx
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { IDM } from '../../typings/db'
 import { ChatWrapper } from './styles'
 import gravatar from 'gravatar'
 import dayjs from 'dayjs'
+import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import regexifyString from 'regexify-string'
 
 interface Props {
   data: IDM
 }
 
 const Chat = ({ data }: Props) => {
+  const { workspace } = useParams<{workspace: string; channel: string}>();
   const user = data.Sender
+
+  const result = useMemo(() => regexifyString({
+    input: data.content,
+    // @[티치냥](7)
+    pattern: /@\[(.+?)\]\((\d+?)\)|\n/g, // id, 줄바꿈
+    decorator(match, index) {
+      const arr = match.match(/@\[(.+?)\]\((\d+?)\)/)!;
+      if(arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+            @{arr[1]}
+          </Link>
+          )
+      }
+      return <br key={index} />
+    }
+  }), [data.content, workspace])
 
   return (
     <ChatWrapper>
@@ -21,7 +42,7 @@ const Chat = ({ data }: Props) => {
           <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('MM/DD hh:mm')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   )
