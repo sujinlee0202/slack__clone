c8f0d1b8ec0c327343ce859c495fa5e2cb24b6fd
titch <sujinlee0202@gmail.com>
2022-12-19 17:08:18 +0900

    reverse infinity scroll

--- slack_front/src/Components/ChatList/index.tsx
+++ slack_front/src/Components/ChatList/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from 'react'
+import { forwardRef, useCallback } from 'react'
 import { IDM } from '../../typings/db'
 import Chat from '../Chat';
 import { ChatZone, Section, StickyHeader } from './styles'
@@ -6,15 +6,26 @@ import { Scrollbars } from 'react-custom-scrollbars-2'
 
 interface Props {
   chatSections: {[key: string]: IDM[]};
+  setSize: (f: (size: number) => number) => Promise<IDM[][] | undefined>
+  isEmpty: boolean;
+  isReachingEnd: boolean;
 }
 
-const ChatList = ({ chatSections }: Props) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, [])
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isEmpty, isReachingEnd }: Props, ref) => {
+  const onScroll = useCallback((values: any) => {
+    if(values.scrollTop === 0 && !isReachingEnd) { // 끝에 도달하면 새로 불러올 필요가 없음
+      console.log('가장 위!')
+      // data 추가 로딩
+      setSize((prevSize) => prevSize + 1)
+      .then(() => {
+        // Scroll 위치 유지
+      })
+    }
+  }, [isReachingEnd, setSize])
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={ref} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date}>
@@ -30,6 +41,6 @@ const ChatList = ({ chatSections }: Props) => {
       </Scrollbars>
     </ChatZone>
   )
-}
+})
 
 export default ChatList
--- slack_front/src/pages/DirectMessage/index.tsx
+++ slack_front/src/pages/DirectMessage/index.tsx
@@ -1,25 +1,31 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useRef } from "react";
 import { Container, Header } from "./styles";
 import gravatar from 'gravatar'
 import ChatBox from "../../Components/ChatBox";
 import ChatList from "../../Components/ChatList";
 import useSWR from "swr";
+import useSWRInfinite from 'swr/infinite'
 import { IDM, IUser } from "../../typings/db";
 import { useParams } from "react-router-dom";
 import fetcher from "../../utils/fetcher";
 import useInput from "../../hooks/useInput";
 import axios from "axios";
 import makeSection from "../../utils/makeSection";
+import Scrollbars from "react-custom-scrollbars-2";
 
 const DirectMessage = () => {
   const { workspace, id} = useParams<{workspace: string, id: string}>();
   const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher)
   const { data: myData } = useSWR(`/api/users`, fetcher)
-  const { data: chatData, mutate: mutateChat } = useSWR<IDM[]>(
-    `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=1`,
+  const { data: chatData, mutate: mutateChat, setSize } = useSWRInfinite<IDM[]>(
+    (index: number) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index+1}`,
     fetcher,
-  )
+  ) // 채팅 가져오는 api
   const [chat, onChangeChat, setChat] = useInput('')
+  const scrollbarRef = useRef<Scrollbars>(null);
+
+  const isEmpty = chatData?.[0]?.length === 0;
+  const isReachingEnd = isEmpty || (chatData && chatData[chatData.length - 1]?.length < 20) || false
 
   const onSubmitForm = useCallback((e: React.FormEvent<HTMLFormElement> | React.KeyboardEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
@@ -42,7 +48,7 @@ const DirectMessage = () => {
     return null;
   }
 
-  const chatSections = makeSection(chatData ? [...chatData].reverse() : [])
+  const chatSections = makeSection(chatData ? [...chatData].flat().reverse() : [])
 
   return (
     <Container>
@@ -50,7 +56,7 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro'})} alt={userData.nickname}></img>
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSections={chatSections} />
+      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isEmpty={isEmpty} isReachingEnd={isReachingEnd}/>
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm}></ChatBox>
     </Container>
   )
