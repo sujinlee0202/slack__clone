d5ae4f328c1befff6b1f01bbbc1563ae71b8a056
titch <sujinlee0202@gmail.com>
2022-12-19 20:14:06 +0900

    submit, then scrollbar's location bottom

--- slack_front/src/pages/DirectMessage/index.tsx
+++ slack_front/src/pages/DirectMessage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { Container, Header } from "./styles";
 import gravatar from 'gravatar'
 import ChatBox from "../../Components/ChatBox";
@@ -15,7 +15,7 @@ import Scrollbars from "react-custom-scrollbars-2";
 
 const DirectMessage = () => {
   const { workspace, id} = useParams<{workspace: string, id: string}>();
-  const { data: userData } = useSWR<IUser>(`/api/workspaces/${workspace}/users/${id}`, fetcher)
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher)
   const { data: myData } = useSWR(`/api/users`, fetcher)
   const { data: chatData, mutate: mutateChat, setSize } = useSWRInfinite<IDM[]>(
     (index: number) => `/api/workspaces/${workspace}/dms/${id}/chats?perPage=20&page=${index+1}`,
@@ -32,17 +32,38 @@ const DirectMessage = () => {
     setChat('')
     console.log('submit')
     console.log(chat)
-    if(chat?.trim()) {
+    
+    if(chat?.trim() && chatData) {
+      const savedChat = chat;
+      mutateChat((prevChatData) => {
+        prevChatData?.[0].unshift({
+          id: (chatData[0][0]?.id || 0) + 1,
+          content: savedChat,
+          SenderId: myData.id,
+          Sender: myData,
+          ReceiverId: userData.id,
+          Receiver: userData,
+          createdAt: new Date(),
+        });
+        return prevChatData
+      }, false)
       axios.post(`/api/workspaces/${workspace}/dms/${id}/chats`, {
         content: chat
       })
       .then(() => {
         mutateChat()
         setChat('')
+        scrollbarRef.current?.scrollToBottom()
       })
       .catch(console.error)
     }
-  }, [setChat, mutateChat, chat, workspace, id])
+  }, [setChat, mutateChat, chat, workspace, id, chatData, myData, userData])
+
+  useEffect(() => {
+    if(chatData?.length === 1) {
+      scrollbarRef.current?.scrollToBottom();
+    }
+  }, [chatData])
 
   if(!userData || !myData) {
     return null;
@@ -56,7 +77,7 @@ const DirectMessage = () => {
         <img src={gravatar.url(userData.email, { s: '24px', d: 'retro'})} alt={userData.nickname}></img>
         <span>{userData.nickname}</span>
       </Header>
-      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isEmpty={isEmpty} isReachingEnd={isReachingEnd}/>
+      <ChatList chatSections={chatSections} ref={scrollbarRef} setSize={setSize} isReachingEnd={isReachingEnd}/>
       <ChatBox chat={chat} onChangeChat={onChangeChat} onSubmitForm={onSubmitForm}></ChatBox>
     </Container>
   )
